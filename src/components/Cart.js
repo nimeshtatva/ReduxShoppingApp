99e82dd27c0fe5a3b8a3fcd84f0ea53a08d91a4d
nimeshtatva <nimesh.parmar@tatvasoft.com>
2022-08-02 18:08:20 +0530

    shopping app

---
+++ src/components/Cart.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { cartActions } from "../store/cart-slice";
+import "./Cart.css";
+const Cart = () => {
+  const dispatch = useDispatch();
+  const showCart = () => {
+    dispatch(cartActions.setShowCart());
+  }
+  const quantity = useSelector((state) => state.cart.totalQuantity);
+  return (
+    <div className="cartIcon">
+      <h3 onClick={showCart}>Cart: {quantity} Items</h3>
+    </div>
+  );
+};
+
+export default Cart;
